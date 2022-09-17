a79969221b134486c68b4c09f514e3ed5af1351c
Kenny <kennysoyoye07@gmail.com>
2022-09-17 10:37:27 +0100

    without react-query

--- src/components/PageTwo.tsx
+++ src/components/PageTwo.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {
+  ChangeEvent,
+  FormEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { ReactComponent as BrandRecognition } from "../svg/icon-brand-recognition.svg";
 import { ReactComponent as FullyCutomizable } from "../svg/icon-fully-customizable.svg";
 import { ReactComponent as DetailedRecords } from "../svg/icon-detailed-records.svg";
@@ -24,15 +30,16 @@ const PageTwo: React.FC<Props> = ({ clicked, setClicked }) => {
 
   const [link, setLink] = useState("");
 
-  const [error, setError] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const [linkArray, setLinkArray] = useState<LinksType[]>([]);
 
+  const [disabled, setDisabled] = useState(true);
+
   const [loading, setLoading] = useState(false);
 
-  const getShortenedLink = async () => {
-    //eslint-disable-next-line
-    const data = await axios
+  const getShortenedLink = () =>
+    axios
       .post(`https://api.shrtco.de/v2/shorten?url=${link}`)
       .then((response) => {
         console.log(response.data.result.short_link);
@@ -46,24 +53,48 @@ const PageTwo: React.FC<Props> = ({ clicked, setClicked }) => {
         setLoading(false);
       })
       .catch((error) => {
+        console.log(error);
         console.log(error.code);
         console.log(error.response.data);
         setLoading(false);
 
         if (error.code === "ERR_NETWORK") {
-          setError("An error occured. Please try again");
-          setTimeout(() => {
-            setError("");
-          }, 3000);
+          handleError("An error occured. Please try again");
         }
 
         if (error.response.data.error_code === 10) {
-          setError("This is a disallowed URL!");
-          setTimeout(() => {
-            setError("");
-          }, 3000);
+          handleError("This is a disallowed URL!");
         }
       });
+
+  const handleError = (error: string) => {
+    setErrorMessage(error);
+    setTimeout(() => {
+      setErrorMessage("");
+    }, 2000);
+  };
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    // clear input field
+    inputRef.current!.value = "";
+
+    // check if link has already been shortened
+    if (linkArray.filter((obj) => obj.longLink === link).length !== 0) {
+      handleError("URL has already been shortened!");
+    } else {
+      getShortenedLink();
+    }
+    setClicked(false);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLink(e.target.value.replace(/\s/g, ""));
+  };
+
+  const handlePaste = () => {
+    setDisabled(false);
   };
 
   useEffect(() => {
@@ -73,77 +104,46 @@ const PageTwo: React.FC<Props> = ({ clicked, setClicked }) => {
 
   useEffect(() => {
     localStorage.setItem("link-array", JSON.stringify(linkArray));
-  });
+  }, [linkArray]);
 
-  if (clicked && null !== inputRef.current) {
-    inputRef.current.focus();
-  }
+  useEffect(() => {
+    setDisabled(inputRef.current?.value.length === 0);
+  }, [inputRef.current?.value]);
+
+  useEffect(() => {
+    clicked && inputRef.current!.focus();
+  }, [clicked]);
+
 
   return (
     <div className="pageTwo">
       <div className="pageTwo-container bg-gray-200 flex flex-col items-center">
         <div className="flex justify-center items-center w-full flex-col relative -mt-20 md:-mt-14 lg:-mt-20">
+          <p className="text-red-400">{linkArray[0]?.shortLink}</p>
           <form
             className="url-container w-3/4 flex flex-col justify-between p-7 rounded-xl bg-cover mb-4 md:flex-row md:items-start lg:py-10 lg:px-16"
             style={{ backgroundImage: `url(${ShortenDesktop})` }}
-            onSubmit={(e) => {
-              e.preventDefault();
-
-              if (
-                null !== inputRef.current &&
-                inputRef.current?.value.length === 0
-              ) {
-                setError("Please provide a url...");
-                setTimeout(() => {
-                  setError("");
-                }, 3000);
-              } else {
-                if (null !== inputRef.current) {
-                  inputRef.current.value = "";
-                }
-
-                if (
-                  linkArray.filter((obj) => obj.longLink === link).length !== 0
-                ) {
-                  setLinkArray([...linkArray]);
-                  document.getElementById("duplicate")!.innerHTML =
-                    "URL has already been shortened!";
-
-                  setTimeout(() => {
-                    document.getElementById("duplicate")!.innerHTML = "";
-                  }, 2000);
-                } else {
-                  setLoading(true);
-                  getShortenedLink();
-
-                  // setLinkArray([
-                  //   ...linkArray,
-                  //   {
-                  //     longLink: link,
-                  //     shortLink: app.shorten(link),
-                  //   },
-                  // ]);
-                }
-                setClicked(false);
-              }
-            }}
+            onSubmit={onSubmit}
           >
             <div className="mb-4 lg:mb-0 md:w-2/3 lg:w-3/4">
               <input
                 ref={inputRef}
-                onChange={(e) => {
-                  setLink(e.target.value);
-                }}
+                onChange={handleChange}
+                onPaste={handlePaste}
                 type="url"
                 placeholder="Shorten a link here..."
                 className="w-full px-3 py-2 rounded-lg mb-2 md:py-4 focus:outline-red-400 focus:placeholder:text-red-300"
               />
-              {<p className="lg:mb-0 text-red-400 italic">{error}</p>}
-              {<p id="duplicate" className="lg:mb-0 text-red-400 italic"></p>}
+              {<p className="lg:mb-0 text-red-400 italic">{errorMessage}</p>}
             </div>
 
             <button
-              className={`flex justify-center items-center bg-teal-400 text-white cursor-pointer rounded-lg py-2 md:w-36 md:h-14 md:font-bold hover:bg-teal-300 transition-all active:scale-105`}
+              disabled={disabled}
+              className={`flex justify-center items-center bg-teal-400 text-white rounded-lg py-2 md:w-36 md:h-14 md:font-bold`.concat(
+                disabled
+                  ? ` cursor-not-allowed opacity-70`
+                  : `cursor-pointer hover:bg-teal-300 transition-all active:scale-105`
+              )}
               type="submit"
             >
               {loading ? (
