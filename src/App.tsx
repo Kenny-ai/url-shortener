1b2114112a39d7a85b00aaa2213f26bad069a8cf
Kenny <kennysoyoye07@gmail.com>
2022-09-17 22:06:03 +0100

    refactored requests with react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import PageOne from './components/PageOne';
-import PageTwo from './components/PageTwo';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import PageOne from "./components/PageOne";
+import PageTwo from "./components/PageTwo";
 
 function App() {
+  const queryClient = new QueryClient();
 
   const [showNav, setShowNav] = useState(false);
 
@@ -15,7 +18,10 @@ function App() {
     <div className="App">
       <Header showNav={showNav} setShowNav={setShowNav} />
       <PageOne showNav={showNav} setClicked={setClicked} />
-      <PageTwo clicked={clicked} setClicked={setClicked} />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <PageTwo clicked={clicked} setClicked={setClicked} />
+      </QueryClientProvider>
       <Footer />
     </div>
   );
